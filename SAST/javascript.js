223790ca2921da819ba60c560d425fc1cb9647f1
gitlabtest24 <162101607+gitlabtest24@users.noreply.github.com>
2024-03-05 15:15:58 +0530

    Update javascript.js

--- SAST/javascript.js
+++ SAST/javascript.js
@@ -4,13 +4,12 @@ async function index(req: Request, res: Response) {
     const { db, User } = req.app.get('sequelize');
 
     try {
-        let loggedInUser = await db.query( //Sink
-            `SELECT * FROM users WHERE user = '${req.query.user}' AND pass = '${req.query.pass}'`, //Source
+        let loggedInUser = await db.query(
+            `SELECT * FROM users WHERE user = '${req.query.user}' AND pass = '${req.query.pass}'`,
             {
                 model: User,
             }
-        ); // Noncompliant
-
+        ); 
         res.send(JSON.stringify(loggedInUser));
     } catch (error) {
         console.error('An error occurred:', error);
